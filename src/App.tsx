428d0b302c5deae5a4fc58717095c4b97817bbef
hania-jas <60855635+hania-jas@users.noreply.github.com>
2021-03-29 10:32:26 +0200

    added local sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // @ts-nocheck
 import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import tick1 from './sounds/tick1.wav';
 import Button from './components/Button';
 import Output from './components/Output';
 import PlayPauseButton from './components/PlayPauseButton';
@@ -8,6 +10,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle } from '@fortawesome/free-solid-svg-icons'
 
 const TIMER_CONST = 60000;
+let audio = new Audio(tick1);
 
 const App = () => {
 const [tempo, setTempo] = useState(60);
@@ -27,13 +30,15 @@ const restartMetronome = () => {
 }
 
  const tick = () => { 
-  let context = new AudioContext()
-  let o = context.createOscillator()
-  let  g = context.createGain()
-  o.connect(g)
-  g.connect(context.destination)
-  o.start(0)
-  o.stop(0.01);
+  
+audio.play();
+console.log(audio.currentTime);
+  // let o = context.createOscillator()
+  // let  g = context.createGain()
+  // o.connect(g)
+  // g.connect(context.destination)
+  // o.start(0)
+  // o.stop(0.01);
 
   setToggleClass(oldClass => !oldClass);
   console.log(toggleClass);
@@ -65,7 +70,7 @@ const togglePlay = () => {
   return (
     <div className="App">
       <h1>METRONOM</h1>
-      <div className={`${toggleClass ? 'show' : 'hide'}`}>
+      <div className={`${toggleClass ? 'show' : 'mask'}`}>
       <FontAwesomeIcon icon={faCircle} className="firstCircle"/>
       <FontAwesomeIcon icon={faCircle} className="secondCircle"/>
       <FontAwesomeIcon icon={faCircle} className="thirdCircle"/>
