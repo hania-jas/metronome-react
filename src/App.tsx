d192eac5dde95b99f2cf5c9159b5787e321488fc
hanna.jasinska <hanna.jasinska@mug.pl>
2021-03-29 15:47:25 +0200

    added icons and some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import React, {useState, useEffect} from 'react';
+import CircularSlider from '@fseehawer/react-circular-slider';  
 import ReactDOM from 'react-dom';
 import './App.css';
 import tick1 from './sounds/tick1.wav';
@@ -7,7 +8,7 @@ import Button from './components/Button';
 import Output from './components/Output';
 import PlayPauseButton from './components/PlayPauseButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCircle, faPlay, faPause, faPlus, faMinus, faRecordVinyl } from '@fortawesome/free-solid-svg-icons'
 
 const TIMER_CONST = 60000;
 let audio = new Audio(tick1);
@@ -16,7 +17,7 @@ const App = () => {
 const [tempo, setTempo] = useState(60);
 const [intervalId, setIntervalId] = useState();
 const [toggleClass, setToggleClass] = useState(false);
-const [buttonName, setButtonName] = useState('play');
+const [buttonName, setButtonName] = useState(<FontAwesomeIcon icon={faPlay}/>);
 
 useEffect(() => {
   restartMetronome();
@@ -30,16 +31,7 @@ const restartMetronome = () => {
 }
 
  const tick = () => { 
-  
-audio.play();
-console.log(audio.currentTime);
-  // let o = context.createOscillator()
-  // let  g = context.createGain()
-  // o.connect(g)
-  // g.connect(context.destination)
-  // o.start(0)
-  // o.stop(0.01);
-
+  audio.play();
   setToggleClass(oldClass => !oldClass);
   console.log(toggleClass);
  }
@@ -59,29 +51,45 @@ console.log(audio.currentTime);
 const togglePlay = () => {
   if (!intervalId) {
    play();
-   setButtonName('pause');
+   setButtonName( <FontAwesomeIcon icon={faPause}/>);
   } else {
    pause();
-   setButtonName('play');
+   setButtonName(<FontAwesomeIcon icon={faPlay}/>);
   }
 }
   
 
   return (
     <div className="App">
-      <h1>METRONOM</h1>
-      <div className={`${toggleClass ? 'show' : 'mask'}`}>
-      <FontAwesomeIcon icon={faCircle} className="firstCircle"/>
-      <FontAwesomeIcon icon={faCircle} className="secondCircle"/>
-      <FontAwesomeIcon icon={faCircle} className="thirdCircle"/>
+      <div className={`animation ${toggleClass ? 'show' : 'mask'}`}>
+        <FontAwesomeIcon icon={faRecordVinyl} className="firstCircle"/>
+        <FontAwesomeIcon icon={faRecordVinyl} className="secondCircle"/>
+        <FontAwesomeIcon icon={faRecordVinyl} className="thirdCircle"/>
       </div>
       <div className="containerApp">
-      <Output val={tempo} />
+        <CircularSlider
+        label={'bpm'}
+        labelBottom={true}
+        knobColor="#434c56"
+        progressColorFrom="#45def0"
+        progressColorTo="#0c6789"
+        trackSize={20}
+        progressSize={20}
+        trackColor="#434c56"
+        width={200}
+        dataIndex={tempo}
+        labelColor="#45def0"
+        min={0}
+        max={400}
+        onChange={(tempo) => { setTempo(tempo)
+        console.log(tempo);}
+        }
+        />
       </div>
       <div className="buttons">
-      <Button value="-" changeValue={() => setTempo(tempo - 1)} />
-      <PlayPauseButton playTheSound={togglePlay} buttonName={buttonName}/>
-      <Button value="+" changeValue={() => setTempo(tempo + 1)}/>
+        <Button value={<FontAwesomeIcon icon={faMinus}/>} changeValue={() => setTempo(tempo - 1)} />
+        <PlayPauseButton playTheSound={togglePlay} buttonName={buttonName}/>
+        <Button value={<FontAwesomeIcon icon={faPlus}/>} changeValue={() => setTempo(tempo + 1)}/>
       </div>
     </div>
   );
